442352b88d945aaa21cf8b4cf5436ca589c76992
Guntram Blohm <gbl@bso2001.com>
2016-03-19 22:41:21 +0100

    first commit

---
+++ src/de/guntram/D7CReader.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.guntram;
+
+/**
+ *
+ * @author gbl
+ */
+public class D7CReader {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
